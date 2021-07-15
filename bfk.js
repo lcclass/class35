208bb8681b2284dfdb62bb007f9b43e3249aa36b
lcclass <81837510+lcclass@users.noreply.github.com>
2021-07-15 16:51:17 +0800

    Add files via upload

---
+++ bfk.js
@@ -0,0 +1,8 @@
+﻿// JavaScript Document
+        function checkpassword(){
+        var pwd="4124";
+                var inpwd=document.getElementById("password").value;
+                        if(pwd == inpwd){  
+                                    window.open("homepage35.html");					
+    }
+}
